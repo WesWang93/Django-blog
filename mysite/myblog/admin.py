2047d2646afaad2f78bf7264e204d05cc0632977
Wesley Wang <wes82@uw.edu>
2018-08-09 19:57:55 -0700

    Lesson7 Commit

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from myblog.models import Post
+from myblog.models import Category
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+class PostInline(admin.TabularInline):
+    model = Post
+
+class CategoryInline(admin.ModelAdmin):
+    inlines = [
+        PostInline,
+    ]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
